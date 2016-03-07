ecebf998f8d9d943d0e5ff6c91d802dda391f341
itzrahulprakash <itzrahulprakash@gmail.com>
2016-03-07 21:37:30 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,85 +12,238 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 #
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
-
-
 BOARD_VENDOR := xiaomi
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8956
-TARGET_NO_BOOTLOADER := true
+LOCAL_PATH := device/xiaomi/kenzo
 
-# Platform
-TARGET_BOARD_PLATFORM := msm8956
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8952
+TARGET_NO_BOOTLOADER := false
+TARGET_NO_KERNEL := false
 
 # Architecture
+ifneq ($(KENZO_32_BIT),true)
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8
-TARGET_CPU_ABI := arm64-v8
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+else
+TARGET_BOARD_SUFFIX := _32
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a72
+endif
+TARGET_BOARD_PLATFORM := msm8952
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
+
+# CPU
+TARGET_CPU_CORTEX_A53 := true
+
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
 # Kernel
-BOARD_KERNEL_CMDLINE :=androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2046
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100 --dt device/xiaomi/kenzo/recovery/dt.img
+BOARD_DTBTOOL_ARGS                 := -2
+BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x00000100 --dt device/xiaomi/kenzo/dt.img
+BOARD_KERNEL_CMDLINE               := androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+BOARD_KERNEL_SEPARATED_DT          := false
+BOARD_KERNEL_BASE                  := 0x80000000
+BOARD_KERNEL_PAGESIZE              := 2048
+BOARD_KERNEL_TAGS_OFFSET           := 0x00000100
+BOARD_RAMDISK_OFFSET               := 0x02000000
+TARGET_PREBUILT_KERNEL		   := device/xiaomi/kenzo/kernel
 
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x68000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xdef7fbc00
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+#ifneq ($(KENZO_32_BIT),true)
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_CONFIG := kenzo_debug_defconfig
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+#TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_USES_UNCOMPRESSED_KERNEL := true
+#else
+#TARGET_KERNEL_CONFIG := cyanogenmod_kenzo_defconfig
+#endif
+#ifneq ($(TARGET_BUILD_VARIANT),user)
+#TARGET_KERNEL_ADDITIONAL_CONFIG := cyanogenmod_debug_config
+#endif
+
+# Audio
+AUDIO_FEATURE_DEEP_BUFFER_RINGTONE := true
+AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
+AUDIO_FEATURE_ENABLED_INCALL_MUSIC := false
+AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
+AUDIO_FEATURE_ENABLED_COMPRESS_CAPTURE := false
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+QCOM_BT_USE_SMD_TTY := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Camera
+# TO DO - Put correct sensors and support
+BOARD_CAMERA_SENSORS := imx135_cp8675 imx214_cp8675 ov5648_cp8675
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Charger
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_SWV8_DISK_ENCRYPTION := true
+
+# Display
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_GRALLOC_USES_ASHMEM := false
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+TARGET_USE_ION_COMPAT := true
+USE_OPENGL_RENDERER := true
+HAVE_ADRENO_SOURCE:= false
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+TARGET_USES_NEW_ION_API := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := device/xiaomi/kenzo/cmhw
+
+# FM
+AUDIO_FEATURE_ENABLED_FM := true
+TARGET_QCOM_NO_FM_FIRMWARE := true
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Dexopt, only if we can fit that in
+ifneq ($(TARGET_TRANSPARENT_COMPRESSION_METHOD),)
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+endif
+
+
+# GPS
+TARGET_GPS_HAL_PATH := $(LOCAL_PATH)/gps
+TARGET_NO_RPC := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# LOGD
+TARGET_USES_LOGD := true
+
+# Malloc
+MALLOC_IMPL := dlmalloc
+
+# Partition
+TARGET_USERIMAGES_USE_EXT4          := true
+BOARD_BOOTIMAGE_PARTITION_SIZE      := 0x02000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 0x02000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 1744830464
+BOARD_USERDATAIMAGE_PARTITION_SIZE  := 13144931840
+BOARD_CACHEIMAGE_PARTITION_SIZE     := 402653184
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE   := ext4
+BOARD_PERSISTIMAGE_PARTITION_SIZE   := 33554432
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE              := 131072 # BOARD_KERNEL_PAGESIZE * 64
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
 
 # Recovery
-
-BOARD_HAS_LARGE_FILESYSTEM := true
-#BOARD_HAS_NO_MISC_PARTITION := true
+RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/root/fstab.qcom
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_USES_MMCUTILS := true
-TARGET_PREBUILT_KERNEL := device/xiaomi/kenzo/recovery/kernel
-TARGET_RECOVERY_INITRC := device/xiaomi/kenzo/etc/init.rc
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#BOARD_INCLUDE_FB2PNG := true
 
-
-DEVICE_RESOLUTION := 1080x1920
-
-# TWRP Build Flags
-#BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
-TW_CUSTOM_THEME :=device/xiaomi/kenzo/twres
-#TW_THEME := portrait_hdpi
-#TW_INCLUDE_DUMLOCK := true
-#TW_INCLUDE_FB2PNG := true
-TW_HAS_DOWNLOAD_MODE := true
+# TWRP-Specific
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_REAL_SDCARD := false
+RECOVERY_SDCARD_ON_DATA := true
 TW_INCLUDE_CRYPTO := true
-TW_NO_EXFAT_FUSE := true
-TW_NO_SCREEN_BLANK := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/xiaomi/kenzo/kernel
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_TARGET_USES_QCOM_BSP := true
+TW_NEW_ION_HEAP := true
 
-# USB
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
+# RIL
+PROTOBUF_SUPPORTED := true
+TARGET_RIL_VARIANT := caf
+ADD_RADIO_FILES := true
 
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/kenzo/sepolicy
+
+# Qualcomm support
+BOARD_USES_QC_TIME_SERVICES := true
+ifneq ($(QCPATH),)
+BOARD_USES_QCNE := true
+TARGET_LDPRELOAD := libNimsWrap.so
+endif
+BOARD_USES_QCOM_HARDWARE := true
+
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+
+# Video
+TARGET_HAVE_SIGNED_VENUS_FW := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+TARGET_USES_QCOM_WCNSS_QMI       := true
+
+
+# inherit from the proprietary version
+-include vendor/xiaomi/kenzo/BoardConfigVendor.mk
